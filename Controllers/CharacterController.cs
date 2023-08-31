2e9fd637e4bf2ff727d42752bd957c99198ec6c6
Oleksandr Holikov <sanyagolikov97@gmail.com>
2023-08-31 12:40:26 +0300

    Character services

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -5,27 +5,28 @@ namespace dotnet_rpg;
 [Route("api/[controller]")]
 public class CharacterController : ControllerBase
 {
-    private static List<Character> characters = new List<Character> {
-        new Character(),
-        new Character{ Id = 1, Name = "Sam"},
-    };
+    private readonly ICharacterService _characterService;
+
+    public CharacterController(ICharacterService characterService)
+    {
+        _characterService = characterService;
+    }
 
     [HttpGet("GetAll")]
     public ActionResult<List<Character>> Get()
     {
-        return Ok(characters);
+        return Ok(_characterService.GetAllCharacters());
     }
 
     [HttpGet("{id}")]
     public ActionResult<List<Character>> GetSingle(int id)
     {
-        return Ok(characters.FirstOrDefault(c => c.Id == id));
+        return Ok(_characterService.GetCharacterById(id));
     }
 
     [HttpPost]
-    public ActionResult<List<Character>> AddCharacter(Character character)
+    public ActionResult<List<Character>> AddCharacter(Character newCharacter)
     {
-        characters.Add(character );
-        return Ok(character);
+        return Ok(_characterService.AddCharacter(newCharacter));
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterService;
 
 internal class Program
 {
@@ -12,6 +13,7 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddScoped<ICharacterService, CharacterService>();
 
         var app = builder.Build();
 
